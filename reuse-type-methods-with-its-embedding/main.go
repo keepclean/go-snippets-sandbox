5eae6d4ae98fb5cbcb40edc114cd2949c5c01b7f
Rustam Iuldashev <rustam.iuldashev@protonmail.com>
2020-01-05 14:27:24 +0000

    new snippet about embedding types

---
+++ reuse-type-methods-with-its-embedding/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	cf := ConfigFile{FileAsset{Path: "xxxx"}}
+	if !cf.Exist() {
+		if err := cf.Create(); err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if err := cf.Read(); err != nil {
+		log.Println(err)
+	}
+}
+
+type FileAsset struct {
+	Path string
+}
+
+func (f FileAsset) String() string {
+	return f.Path
+}
+
+func (f FileAsset) Exist() bool {
+	_, err := os.Stat(f.ExpandPath())
+	if err != nil && os.IsNotExist(err) {
+		return false
+	} else if err != nil {
+		log.Println("non-IsNotExist error upon calling os.Stat:", err)
+		return false
+	}
+
+	return true
+}
+
+func (f FileAsset) ExpandPath() string {
+	return os.ExpandEnv(f.String())
+}
+
+func (f FileAsset) DirName() string {
+	return filepath.Dir(f.ExpandPath())
+}
+
+func (f FileAsset) Read() error {
+	data, err := ioutil.ReadFile(f.ExpandPath())
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(string(data))
+	return nil
+}
+
+type ConfigFile struct {
+	FileAsset
+}
+
+func (cf ConfigFile) Create() error {
+	fd, err := os.Create(cf.Path)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	log.Printf("file %s has been created\n", cf.Path)
+	return nil
+}
